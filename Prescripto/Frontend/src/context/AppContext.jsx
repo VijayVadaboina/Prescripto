14834bded40e050b79a6a3d35e37bbb01a9aedbb
VijayVadaboina <vijayvvadaboina@gmail.com>
2025-01-26 23:28:17 +0530

    FE added doctors details section in appointment page

--- Prescripto/Frontend/src/context/AppContext.jsx
+++ Prescripto/Frontend/src/context/AppContext.jsx
@@ -3,9 +3,12 @@ import { doctors } from "../assets/assets";
 
 export const AppContext = createContext();
 const AppContextProvider = (props) => {
+  const currencySymbol = "$";
   const value = {
     doctors,
+    currencySymbol,
   };
+
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
--- Prescripto/Frontend/src/pages/Appointment.jsx
+++ Prescripto/Frontend/src/pages/Appointment.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 export default function Appointment() {
+  const { docId } = useParams();
+  const { doctors, currencySymbol } = useContext(AppContext);
+  const [docInfo, setDocInfo] = useState(null);
+  const fetchDocInfo = async () => {
+    const docInf = await doctors.find((doc) => doc._id === docId);
+    setDocInfo(docInf);
+  };
+  useEffect(() => {
+    fetchDocInfo();
+  }, [doctors, docId]);
   return (
-    <>
-      <h1>appontment page</h1>
-    </>
+    docInfo && (
+      <div className="flex flex-col sm:flex-row gap-4">
+        {/* --doc info --*/}
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div>
+            <img
+              className="bg-primary w-full sm:max-w-72 rounded-lg"
+              src={docInfo.image}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+          {/* docs name and details */}
+          <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+            {docInfo.name}
+            <img className="w-5" src={assets.verified_icon} />
+          </p>
+          <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+            <p>
+              {docInfo.degree} - {docInfo.speciality}
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {docInfo.experience}
+              </button>
+            </p>
+          </div>
+          {/* doctors about */}
+          <div>
+            <p className="flex item-center gap-1 text-sm font-medium text-gray-900 mt-3">
+              About <img src={assets.info_icon} />
+            </p>
+            <p className="text-sm text-gray-500 max-w-[700px] mt-1">
+              {docInfo.about}
+            </p>
+          </div>
+          <p className="text-gray-500 font-medium mt-4">
+            Appointment fee:
+            <span className="text-gray-600">
+              {currencySymbol}
+              {docInfo.fees}
+            </span>
+          </p>
+        </div>
+      </div>
+    )
   );
 }
