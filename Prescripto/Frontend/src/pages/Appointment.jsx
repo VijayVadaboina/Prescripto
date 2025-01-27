e2048cefb3c8b665e437e2979cdbfb7fc5e30588
VijayVadaboina <vijayvvadaboina@gmail.com>
2025-01-27 23:45:51 +0530

    FE added related doctors functionality in appointment page

--- Prescripto/Frontend/src/pages/Appointment.jsx
+++ Prescripto/Frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/assets";
+import RelatedDoctors from "../components/RelatedDoctors";
 
 export default function Appointment() {
   const { docId } = useParams();
@@ -60,48 +61,49 @@ export default function Appointment() {
   }, docSlots);
   return (
     docInfo && (
-      <div className="flex flex-col sm:flex-row gap-4">
+      <div className="flex flex-col sm:flex-col gap-4">
         {/* --doc info --*/}
-        <div className="flex flex-col sm:flex-row gap-4">
-          <div>
+        <div className="flex flex-row gap-2">
+          <div className="flex flex-col sm:flex-row gap-4">
             <img
               className="bg-primary w-full sm:max-w-72 rounded-lg"
               src={docInfo.image}
               alt=""
             />
           </div>
-        </div>
-        <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
-          {/* docs name and details */}
-          <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
-            {docInfo.name}
-            <img className="w-5" src={assets.verified_icon} />
-          </p>
-          <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
-            <p>
-              {docInfo.degree} - {docInfo.speciality}
-              <button className="py-0.5 px-2 border text-xs rounded-full">
-                {docInfo.experience}
-              </button>
+          <div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+            {/* docs name and details */}
+            <p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+              {docInfo.name}
+              <img className="w-5" src={assets.verified_icon} />
+            </p>
+            <div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+              <p>
+                {docInfo.degree} - {docInfo.speciality}
+                <button className="py-0.5 px-2 border text-xs rounded-full">
+                  {docInfo.experience}
+                </button>
+              </p>
+            </div>
+            {/* doctors about */}
+            <div>
+              <p className="flex item-center gap-1 text-sm font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} />
+              </p>
+              <p className="text-sm text-gray-500 max-w-[700px] mt-1">
+                {docInfo.about}
+              </p>
+            </div>
+            <p className="text-gray-500 font-medium mt-4">
+              Appointment fee:
+              <span className="text-gray-600">
+                {currencySymbol}
+                {docInfo.fees}
+              </span>
             </p>
           </div>
-          {/* doctors about */}
-          <div>
-            <p className="flex item-center gap-1 text-sm font-medium text-gray-900 mt-3">
-              About <img src={assets.info_icon} />
-            </p>
-            <p className="text-sm text-gray-500 max-w-[700px] mt-1">
-              {docInfo.about}
-            </p>
-          </div>
-          <p className="text-gray-500 font-medium mt-4">
-            Appointment fee:
-            <span className="text-gray-600">
-              {currencySymbol}
-              {docInfo.fees}
-            </span>
-          </p>
         </div>
+
         {/* Booking slots */}
         <div className="sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700">
           <p>Booking Slots</p>
@@ -143,6 +145,8 @@ export default function Appointment() {
             Book an appointment
           </button>
         </div>
+        {/* related doctors */}
+        <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
       </div>
     )
   );
