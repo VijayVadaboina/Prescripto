d02cbb12e07ffc400441bce8893be413d2c911ad
VijayVadaboina <vijayvvadaboina@gmail.com>
2025-02-08 23:45:27 +0530

    BE added logic for adding a doctor by admin

--- Prescripto/Backend/controllers/adminController.js
+++ Prescripto/Backend/controllers/adminController.js
@@ -1,3 +1,8 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+import doctorModel from "../models/doctorModel.js";
+
 const addDoctor = async (req, res) => {
   try {
     const {
@@ -23,9 +28,44 @@ const addDoctor = async (req, res) => {
       !fees ||
       !address
     ) {
-      return res.json;
+      return res.json({ success: flase, message: "Missing Details" });
     }
-  } catch (error) {}
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Invalid mail address" });
+    }
+    if (password.length < 6) {
+      return res.json({
+        success: false,
+        message: "please choose a strong password",
+      });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
+    const imageUrl = imageUpload.secure_url;
+    const doctorData = {
+      name,
+      email,
+      image: imageUrl,
+      password: hashedPassword,
+      speciality,
+      degree,
+      experience,
+      about,
+      fees,
+      address: JSON.parse(address),
+      date: Date.now(),
+    };
+    const newDoctor = new doctorModel(doctorData);
+    await newDoctor.save();
+    res.json({ success: true, message: "Doctor added Successfully" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error });
+  }
 };
 
 export { addDoctor };
