44e3ea8bc86fbf6dfb45a1c9a6f4dcb3fdc3f6c6
VijayVadaboina <vijayvvadaboina@gmail.com>
2025-02-11 21:53:41 +0530

    BE added Login dashboard with toastify for Admin

--- Prescripto/Admin/src/App.jsx
+++ Prescripto/Admin/src/App.jsx
@@ -1,10 +1,22 @@
+import { useContext } from "react";
 import Login from "./pages/Login";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { AdminContext } from "./context/AdminContext";
 
 function App() {
-  return (
-    <div className="text-green-500">
-      <Login />
+  const { aToken } = useContext(AdminContext);
+
+  return aToken ? (
+    <div className="">
+      <ToastContainer />
     </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
   );
 }
 
--- Prescripto/Admin/src/pages/Login.jsx
+++ Prescripto/Admin/src/pages/Login.jsx
@@ -1,3 +1,84 @@
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import { AdminContext } from "../context/AdminContext";
+
 export default function Login() {
-  return <div>console.log(ndj); dcndjn</div>;
+  const [state, setState] = useState("Admin");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setAToken, backendUrl } = useContext(AdminContext);
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    try {
+      if (state === "admin") {
+        const { data } = await axios.post(backendUrl + "/api/admin/login", {
+          email,
+          password,
+        });
+        if (data.success) {
+          localStorage.setItem("aToken", data.token);
+          setAToken(data.token);
+        } else {
+          toast.error(data.message);
+        }
+      }
+    } catch (e) {}
+  };
+  return (
+    <form
+      onSubmit={onSubmitHandler}
+      action=""
+      className="min-h-[80vh] flex items-center"
+    >
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-#5E5E5E text-sm shadow-lg">
+        <p className="text-2xl font-semibold m-auto">
+          <span className="text-primary">{state}</span> Login
+        </p>
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="email"
+            required
+          />
+        </div>
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className="border border-[#DADADA] rounded w-full p-2 mt-1"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            type="password"
+            required
+          />
+        </div>
+        <button className="bg-primary text-white w-full py-2 rounded-md text-base">
+          Login
+        </button>
+        {state === "Admin" ? (
+          <p>
+            Doctor Login?{" "}
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Doctor")}
+            >
+              Click here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Admin Login?{" "}
+            <span
+              className="text-primary underline cursor-pointer"
+              onClick={() => setState("Admin")}
+            >
+              Click here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
 }
