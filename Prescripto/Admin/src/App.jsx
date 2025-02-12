9e01e65683f930272ce8fb777dbe2301af09a095
VijayVadaboina <vijayvvadaboina@gmail.com>
2025-02-12 22:08:04 +0530

    BE added Navbar component for admin dashboard page

--- Prescripto/Admin/src/App.jsx
+++ Prescripto/Admin/src/App.jsx
@@ -4,13 +4,15 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
 
 function App() {
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <div className="">
+    <div className="bg-[#F8F9Fd]">
       <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
