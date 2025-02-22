3f14bddb185f9405943533993c8b9d7b3fb6c6ab
VijayVadaboina <vijayvvadaboina@gmail.com>
2025-02-22 18:20:44 +0530

    adding doctors list page in admin panel

--- Prescripto/Admin/src/context/AdminContext.jsx
+++ Prescripto/Admin/src/context/AdminContext.jsx
@@ -1,16 +1,36 @@
 import { createContext, useState } from "react";
-
+import axios from "axios";
+import { toast } from "react-toastify";
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
   );
+  const [doctors, setDoctors] = useState([]);
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/all-doctors",
+        {},
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
   const value = {
     aToken,
     setAToken,
     backendUrl,
+    getAllDoctors,
+    doctors,
   };
   console.log(value);
   return (
