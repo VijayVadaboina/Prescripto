df255b01fda783719869bb1bc97f5ba8855a1b92
VijayVadaboina <vijayvvadaboina@gmail.com>
2025-02-23 17:31:41 +0530

    added update profile api to user controller

--- Prescripto/Backend/routes/userRoute.js
+++ Prescripto/Backend/routes/userRoute.js
@@ -3,6 +3,7 @@ import {
   registerUser,
   loginUser,
   getProfile,
+  updateProfile,
 } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
 
@@ -11,5 +12,11 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post(
+  "/update-profile",
+  authUser,
+  upload.single("image"),
+  updateProfile
+);
 
 export default userRouter;
